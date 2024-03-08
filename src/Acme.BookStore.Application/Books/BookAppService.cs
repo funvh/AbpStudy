a7b01822510ee987271d11632586f89eb448bdcc
funvh <funv_h@163.com>
2024-03-08 13:50:19 +0800

    1

---
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,19 @@
+﻿using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService : CrudAppService<
+            Book, //The Book entity
+            BookDto, //Used to show books
+            long, //Primary key of the book entity
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateBookDto>,
+        IBookAppService
+    {
+        public BookAppService(IRepository<Book, long> repository) : base(repository)
+        {
+        }
+    }
+}
