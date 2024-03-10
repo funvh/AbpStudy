fe4a09f9c37e82137cfc012d37bf84dc1bf2ea2f
funvh <funv_h@163.com>
2024-03-11 00:02:53 +0800

    1

--- src/Acme.BookStore.Web/Pages/Books/Index.js
+++ src/Acme.BookStore.Web/Pages/Books/Index.js
@@ -1,5 +1,7 @@
 $(function () {
     var l = abp.localization.getResource('BookStore');
+    var createModal = new abp.ModalManager(abp.appPath + 'Books/CreateModal');
+    var editModal = new abp.ModalManager(abp.appPath + 'Books/EditModal');
 
     var dataTable = $('#BooksTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
@@ -10,6 +12,33 @@ $(function () {
             scrollX: true,
             ajax: abp.libs.datatables.createAjax(acme.bookStore.books.book.getList),
             columnDefs: [
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items: [
+                            {
+                                text: l('Edit'),
+                                action: function (data) {
+                                    editModal.open({ id: data.record.id });
+                                }
+                            },
+                            {
+                                text: l('Delete'),
+                                confirmMessage: function (data) {
+                                    return l('BookDeletionConfirmationMessage', data.record.name);
+                                },
+                                action: function (data) {
+                                    acme.bookStore.books.book
+                                        .delete(data.record.id)
+                                        .then(function () {
+                                            abp.notify.info(l('SuccessfullyDeleted'));
+                                            dataTable.ajax.reload();
+                                        });
+                                }
+                            }
+                        ]
+                    }
+                },
                 {
                     title: l('Name'),
                     data: "name"
@@ -50,9 +79,11 @@ $(function () {
         })
     );
 
-    var createModal = new abp.ModalManager(abp.appPath + 'Books/CreateModal');
-
     createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    editModal.onResult(function () {
         dataTable.ajax.reload();
     });
 
